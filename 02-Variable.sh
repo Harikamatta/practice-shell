4823a27f2e74bb3e6111fa17f7a3236f80a9d093
Harikamatta <harikamattanjc@gmail.com>
2023-12-14 14:43:51 +0530

    updating names

--- 02-Variable.sh
+++ 02-Variable.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 PERSON1=Harika
+
 PEROSN2=Deepika
 
 
 echo "$PERSON1 : Hello $PERSON2 , Good Morning"
 echo "$PERSON2 : Hello $PERSON1 , Good Morning"
 echo "$PERSON1 : Hello $PERSON2 , How are you?"
-echo "$PERSON2 : Hello $PERSON1 , I'm doing great, howv are you doing?"
+echo "$PERSON2 : Hello $PERSON1 , I'm doing great, how are you doing?"
 
 
