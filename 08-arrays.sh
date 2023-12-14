c4dd3a95df719dfda9ea790472d6e0064a1b885b
Harikamatta <harikamattanjc@gmail.com>
2023-12-14 15:30:26 +0530

    division

---
+++ 08-arrays.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo"Enter first number: "
+read num1
+
+echo "Enter second number :"
+read num2
+
+#perform division
+result=$((num1/num2))
+
+# Display the result
+echo "The result of $num1 / $num2 is: $result"
+
+
