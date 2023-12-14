22ce5e8578bc016f0e1049fc338c74ffbc9a0918
Harikamatta <harikamattanjc@gmail.com>
2023-12-14 14:50:28 +0530

    variables

---
+++ 04-Variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$1=Harika
+$2=Deepika
+
+echo "$1 : Hello $2 , Good Morning"
+echo "$2 : Hello $1 , Good Morning"
+echo "$1 : Hello $2 , How are you?"
+echo "$2 : Hello $1 , I'm doing great, how are you doing?"
