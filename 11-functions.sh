9926c56d4be1bd567e8e291b95afb7af87e28792
Harikamatta <harikamattanjc@gmail.com>
2023-12-15 07:06:22 +0530

    functions

---
+++ 11-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ID=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+    echo "Error: : $2 ...FAILED"
+    else
+    echo "$2 ...SUCCESS"
+    fi
+}
+    if [ $ID -ne 0 ]
+    then
+    echo "Error not a root user"
+    exit  1
+    else 
+    echo "root user"
+    fi
+
+    yum install mysql -y
+    VALIDATE $? "Installing MYSQL"
+    yum install git -y
+    VALIDATE $? "Installing GIT"
