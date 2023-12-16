9736225ab2af7daf0ccfabe4469ca8f1b27db5ed
Harikamatta <harikamattanjc@gmail.com>
2023-12-16 21:13:59 +0530

    installations

--- 10-installations.sh
+++ 10-installations.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
 ID=$(id -u)
-    if [ $ID -ne 0 ]
-    then
-    echo "Error not a root user"
-    exit  1
-    else 
-    echo "root user"
-    fi
-
-    yum install mysql -y
-    if [ $ID -ne 0 ]
-    then 
-    echo "Error: : installing MYSQL failed"
-    else
-    echo "installing mysql success"
-    fi
-    yum install git -y
-    if [ $ID -ne 0 ]
-    then 
-    echo "Error: : installing GIT failed"
-    else
-    echo "installing GIT success"
-    fi
+if [ ID -ne 0 ]
+then 
+echo "Error not a root user"
+else 
+echo "root user"
+fi
