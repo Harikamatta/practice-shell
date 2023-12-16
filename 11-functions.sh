b5005cbf36f951a17ed039b4f2d80bec8d4ced63
Harikamatta <harikamattanjc@gmail.com>
2023-12-16 21:33:16 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ID=$(id -u)
-VALIDATE() {
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
     echo "Error: : $2.. failed"
     exit 1
     else 
     echo "$2 installed successfully"
-}
+    fi }
     if [ $ID -ne 0 ]
     then 
     echo "Error not a root user"
