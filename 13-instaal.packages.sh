dbd4c9bc487d03a18a25b43ff47d50fc6af91e12
Harikamatta <harikamattanjc@gmail.com>
2023-12-16 22:53:07 +0530

    packages

--- 13-instaal.packages.sh
+++ 13-instaal.packages.sh
@@ -7,10 +7,17 @@ R="/e[31m"
 G="/e[32m"
 Y="/e[e33m"
 N="/e[0m"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+    echo -e "$2 .. $R FAILED $N"
+    else
+    echo -e "$2 .. $G SUCCESS $N"
+}
 
 if [ $ID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
+    echo -e "$R ERROR:: Please run this script with root access $N" 
     exit 1 # you can give other than 0
 else
     echo "You are root user"
