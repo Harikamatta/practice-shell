dadecc7804ee6d153b39ad1604c121f84d34ab6e
Harikamatta <harikamattanjc@gmail.com>
2023-12-16 21:21:18 +0530

    installations

--- 10-installations.sh
+++ 10-installations.sh
@@ -6,3 +6,15 @@ echo "Error not a root user"
 else 
 echo "root user"
 fi
+yum install mysql -y
+if [$ID -ne 0 ]
+echo "Installing of mysql failed"
+else 
+echo "installed mysql successfully"
+fi
+yum install git -y
+if [$ID -ne 0 ]
+echo "Installing of git failed"
+else 
+echo "installed git successfully"
+fi
