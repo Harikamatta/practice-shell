ad554c17fad640589e595f31979c7c7aeff26d81
Harikamatta <harikamattanjc@gmail.com>
2023-12-16 21:21:59 +0530

    installations

--- 10-installations.sh
+++ 10-installations.sh
@@ -8,12 +8,14 @@ echo "root user"
 fi
 yum install mysql -y
 if [$ID -ne 0 ]
+then
 echo "Installing of mysql failed"
 else 
 echo "installed mysql successfully"
 fi
 yum install git -y
 if [$ID -ne 0 ]
+then
 echo "Installing of git failed"
 else 
 echo "installed git successfully"
